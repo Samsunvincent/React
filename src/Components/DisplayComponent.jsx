10684a6284a690c2f0b76e2ea546c4c3a2af9987
Samsunvincent <samsunvincent143@gmail.com>
2024-10-23 11:26:01 +0530

    multiple input types

--- src/Components/DisplayComponent.jsx
+++ src/Components/DisplayComponent.jsx
@@ -1,7 +1,13 @@
 export default function DisplayComponent({displayData}){
     return(
         <>
-            <h1>Data : {displayData}</h1>
+           <h1>Name : {displayData.name}</h1>
+           <h1>Email : {displayData.email}</h1>
+           <h1>place : {displayData.place}</h1>
+           <h1>pincode : {displayData.pincode}</h1>
+
+
+
         </>
     )
 }
--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -2,11 +2,52 @@ import { useState } from "react"
 
 export default function Formcomponent({onSubmit}){
 
-    const[inputData , setInputData] = useState(null)
+    const[inputData , setInputData] = useState({
+        name : '',
+        email : '',
+        place : '',
+        pincode : '',
+    });
 
     const handleInputChange = (e)=>{
-        console.log('value',e.target.value)
-        setInputData(e.target.value)
+      
+        // setInputData(e.target.value)
+
+        if(e.target.name === "name"){
+            console.log("name",e.target.value);
+
+            setInputData({
+                ...inputData,
+                name : e.target.value,
+            })
+        }
+        if(e.target.name === "email"){
+            console.log("email",e.target.value);
+
+            setInputData({
+                ...inputData,
+                email : e.target.value
+            })
+
+        }
+        if(e.target.name === "place"){
+            console.log("place",e.target.value);
+
+            setInputData({
+                ...inputData,
+                place : e.target.value
+            })
+
+        }
+        if(e.target.name === "pincode"){
+            console.log("pincode",e.target.value);
+
+            setInputData({
+                ...inputData,
+                pincode : e.target.value
+            })
+
+        }
        
     }
 
@@ -20,7 +61,11 @@ export default function Formcomponent({onSubmit}){
     return(
         <>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleInputChange}/>
+                <input type="text" name="name" placeholder ="Enter your name "onChange={handleInputChange}/>
+                <input type="email" name="email" placeholder="Enter your email"  onChange={handleInputChange}/>
+                <input type="text" name="place" placeholder="Enter your place"  onChange={handleInputChange}/>
+                <input type="text" name="pincode" placeholder="Enter your pincode"  onChange={handleInputChange}/>
+
                 <button type="submit">Submit</button>
             </form>
         </>
